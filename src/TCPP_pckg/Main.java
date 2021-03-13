327436c673fc38d746d370f24d096ab00c87593d
nc6d <6od92002@gmail.com>
2021-03-13 17:57:19 +0200

    change_2

--- src/TCPP_pckg/Main.java
+++ src/TCPP_pckg/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("CHANGE_1(ALPHA)");
+        System.out.println("CHANGE_2(ALPHA)");
     }
 }
