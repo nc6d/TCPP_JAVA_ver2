cd090697104b382e02ee65b1a6191db95a9d4e64
nc6d <6od92002@gmail.com>
2021-03-17 15:08:21 +0200

    changed from ide

--- src/TCPP_pckg/Main.java
+++ src/TCPP_pckg/Main.java
@@ -3,7 +3,7 @@ package TCPP_pckg;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! (from ide)");
         System.out.println("CHANGE_1(ALPHA)");
         System.out.println("CHANGE_2(ALPHA)");
         System.out.println("CHANGES FROM BROWSER (ALPHA) ");
