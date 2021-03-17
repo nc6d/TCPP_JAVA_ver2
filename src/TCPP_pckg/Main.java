900773c00cc6497feea23e8cced8445ff7348723
Bohdan Opanasyuk <58623074+nc6d@users.noreply.github.com>
2021-03-17 15:07:47 +0200

    changed from browser

--- src/TCPP_pckg/Main.java
+++ src/TCPP_pckg/Main.java
@@ -3,9 +3,10 @@ package TCPP_pckg;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! (from browser");
         System.out.println("CHANGE_1(ALPHA)");
         System.out.println("CHANGE_2(ALPHA)");
         System.out.println("CHANGES FROM BROWSER (ALPHA) ");
+        
     }
 }
